ca8b10eeffce88e57f8559c5eb9bfe73b32ba8f9
zbun-karina <vt211_zks@student.ztu.edu.ua>
2024-03-11 22:06:56 +0200

    feat: add navigation

--- lab/app/articles/layout.tsx
+++ lab/app/articles/layout.tsx
@@ -1,9 +1,44 @@
+'use client'
 import {PropsWithChildren} from "react";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import classNames from "classnames";
+
+const articlesRoutes = [
+    {
+        title: 'Favourite',
+        path: '/articles/favourite'
+    },
+    {
+        title: 'Create',
+        path: '/articles/create'
+    }
+]
 
 export default function ArticlesLayout({children}: PropsWithChildren) {
+
+    const pathname = usePathname()
+
     return (
         <>
-            ArticlesLayout
+            <nav>
+                <ul className="flex gap-2">
+                    {
+                        articlesRoutes.map(({title, path}, index) => (
+                            <menu key={index}>
+                                <Link
+                                    href={path}
+                                    className={classNames('text-2xl underline', {
+                                        'text-red-500': pathname === path
+                                    })}
+                                >
+                                    {title}
+                                </Link>
+                            </menu>
+                        ))
+                    }
+                </ul>
+            </nav>
             <hr/>
             {children}
         </>
