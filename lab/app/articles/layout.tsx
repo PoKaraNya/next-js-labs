72572ac6e51f970e5e3ae4a525c5877c2d109b72
zbun-karina <vt211_zks@student.ztu.edu.ua>
2024-03-14 23:59:54 +0200

    feat: add scss

--- lab/app/articles/layout.tsx
+++ lab/app/articles/layout.tsx
@@ -3,6 +3,8 @@ import {PropsWithChildren} from "react";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 import classNames from "classnames";
+import "@/styles/modules/layout.scss"
+import s from "@/styles/modules/menu.module.scss"
 
 const articlesRoutes = [
     {
@@ -20,27 +22,27 @@ export default function ArticlesLayout({children}: PropsWithChildren) {
     const pathname = usePathname()
 
     return (
-        <>
+        <main className="layout">
             <nav>
                 <ul className="flex gap-2">
                     {
                         articlesRoutes.map(({title, path}, index) => (
-                            <menu key={index}>
+                            <li key={index} className={s.link}>
                                 <Link
                                     href={path}
                                     className={classNames('text-2xl underline', {
-                                        'text-red-500': pathname === path
+                                        [s.link_active]: pathname === path
                                     })}
                                 >
                                     {title}
                                 </Link>
-                            </menu>
+                            </li>
                         ))
                     }
                 </ul>
             </nav>
             <hr/>
             {children}
-        </>
+        </main>
     )
 }
